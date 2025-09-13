56e3c4890207d4179209d9180fa3364dbe46bad2
FabianIvar <raaf4862@proton.me>
2025-09-14 01:46:22 +0200

    commit : modified playsound.py

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,18 +1,30 @@
 from pygame import mixer
 import time
 
-
 mixer.init()
-mixer.music.load("playsoundFiles/snare.mp3")
-mixer.music.set_volume(0.7)
+snare = mixer.Sound("playsoundFiles/snare.wav")
+
+#https://stackoverflow.com/questions/5996881/how-to-limit-a-number-to-be-within-a-specified-range-python#5996949
+def const(n, min, max):
+    if n < min:. venv/bin/activate
+        property
+        return min
+    elif n > max:. venv/bin/activate
+        property
+        return max
+    else:
+        return n
+
 amt = int(input("Number of repetitions: "))
 spd = float(input("Time in seconds between repetitions:"))
+vol = const(float(input("Gain (value between 0-1):")), 0, 1)
 
-
+snare.set_volume(vol)
 
 for i in range(amt):
   print(i+1)
-  mixer.music.play()
+  snare.play()
   time.sleep(spd)
 else:
     print("Finished!")
+    time.sleep(snare.get_length())

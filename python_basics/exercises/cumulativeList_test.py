303dd51cb88b704594af81db2e2eb7830be4d644
FabianIvar <raaf4862@proton.me>
2025-10-11 00:45:11 +0200

    modified cumulativeList_test.py: cleaned up code and removed unnecessary comments

--- python_basics/exercises/cumulativeList_test.py
+++ python_basics/exercises/cumulativeList_test.py
@@ -1,19 +1,15 @@
-"""
-make a cumulative_list that for every index in input_list
-adds the current index of input_list + all the values from the previous indexes."""
-
 def cumulative(input_list):
-
-    # Make empty list to store values in
-    cumulative_list = []
+    """
+    make a list that for every index in input_list
+    adds the current index of input_list + the previous value.
+    """
+    timestamps = []
 
     for i in range(len(input_list)):
-        for j in range(i+1):
-            cumulative_list.append(sum([input_list[j]]))
+        timestamps.append(sum([input_list[j] for j in range(i)]))
 
-    print(cumulative_list)
-    return cumulative_list
+    return timestamps
 
 testList = [4,8,1,3]
 
-# print(sumList(testList, 4))
+print(cumulative(testList))

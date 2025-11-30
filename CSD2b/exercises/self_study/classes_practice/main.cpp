ac50cb2a6d12840b26cd93df01f122a9c07f589b
FabianIvar <raaf4862@proton.me>
2025-11-30 22:31:37 +0100

    continued testing with classes and inheritance, playing with pointers >:)

--- CSD2b/exercises/self_study/classes_practice/main.cpp
+++ CSD2b/exercises/self_study/classes_practice/main.cpp
@@ -5,12 +5,15 @@ class Pet {
 public:
   Pet(string name);
   void action();
+  void drink();
 protected:
   string name;
 };
 
 Pet::Pet(string name) {
-  cout << "Constructor Pet\n" << endl;
+  cout << "\n===============" << endl;
+  cout << "Constructor Pet" << endl;
+  cout << "===============\n" << endl;
   cout << "My pet is called " << name << endl;
   this->name = name;
 }
@@ -19,13 +22,31 @@ void Pet::action() {
   cout << name << " does a flip!" << endl;
 }
 
+void Pet::drink() {
+  cout << name << " drinks some water.." << endl;
+}
+
+int species_to_int(string species) {
+  if (species.compare("dog") == 0) {
+    // cout << "input was dog" << endl;
+    return 1;
+  }
+  else if (species.compare("cat") == 0) {
+    // cout << "input was cat" << endl;
+    return 2;
+  }
+  else {
+    return 0;
+  }
+}
+
 class Dog : public Pet {
 public:
   Dog(string name);
-  void bark() {
+  void sound() {
     cout << name <<" stays silent" << endl;
   };
-  void bark(int amount); // method overloading!
+  void sound(int amount); // method overloading!
 };
 
 /*
@@ -34,12 +55,15 @@ dit is hoe je zorgt dat name weer gebruikt kan worden in alle functies doordat j
 die 'this->' pointer
 */
 
+// constructor delegation
 Dog::Dog(string name) : Pet(name) {
-  cout << "constructor Dog\n" << endl;
-  cout << "My dog is called " << name << endl;;
+  cout << "\n===============" << endl;
+  cout << "constructor Dog" << endl;
+  cout << "===============\n" << endl;
+  cout << name << " is a dog" << endl;
 }
 
-void Dog::bark(int amount) {
+void Dog::sound(int amount) {
   cout << name << ": ";
   for (int i = 0; i < amount; i++) {
     if (i == 0) {
@@ -51,10 +75,77 @@ void Dog::bark(int amount) {
   cout << "'" << endl;
 }
 
+// Is it better to ask for pet name in base class constructor?
+// Or is it okay to set the name in the main() function?
+
+class Cat : public Pet {
+public:
+  Cat(string name);
+  void sound(int amount);
+};
+
+Cat::Cat(string name) : Pet(name) {
+  cout << "\n===============" << endl;
+  cout << "constructor Cat" << endl;
+  cout << "===============\n" << endl;
+  cout << "is a cat" << endl;
+}
+
+// duplicate code, but doesn't matter for this test
+void Cat::sound(int amount) {
+  cout << name << ": ";
+  for (int i = 0; i < amount; i++) {
+    if (i == 0) {
+      cout << "'meow";
+    } else {
+      cout << " meow";
+    }
+  };
+  cout << "'" << endl;
+}
+
 int main() {
-  Dog myPet("George");
-  myPet.action();
-  myPet.bark();
-  myPet.bark(6);
+  string petName;
+  cout << "I have a pet called ";
+  getline(cin, petName);
+
+  string species;
+  cout << petName << " is a ";
+  getline(cin, species);
+
+  int species_as_int = species_to_int(species);
+
+
+// Switch case doesn't work for creating objects?
+  // switch (species_as_int) {
+  //   case 1:
+  //     Dog myPet(petName);
+  //     cout << "dog" << endl;
+  //     break;
+  //   case 2:
+  //     // Cat myPet(petName);
+  //     cout << "cat" << endl;
+  //     break;
+  //   default:
+  //     cout << "Doesn't know the species '" << species << "'" << endl;
+  //     break;
+  // }
+
+  if (species_as_int == 1) {
+    Dog myPet(petName);
+    Dog* ptr = &myPet;
+  }
+  else if (species_as_int == 2) {
+    Cat myPet(petName);
+    Cat* ptr = &myPet;
+  }
+  else if (species_as_int == 0) {
+    cout << "Invalid input... closing program" << endl;
+  };
+
+  *ptr.action();
+  *ptr.sound();
+  *ptr.sound(6);
+  *ptr.drink();
   return 0;
 }

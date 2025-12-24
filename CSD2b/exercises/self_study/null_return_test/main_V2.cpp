c2de11ebb12caf403095211605f7d62ae91a81bf
FabianIvar <raaf4862@proton.me>
2025-12-24 02:52:24 +0100

    Experimented some more, tuples with an unpack function are the way to go I think

--- CSD2b/exercises/self_study/null_return_test/main_V2.cpp
+++ CSD2b/exercises/self_study/null_return_test/main_V2.cpp
@@ -6,7 +6,7 @@ Posted by Kartik: https://www.geeksforgeeks.org/profile/kartik?tab=overview
 #include <iostream>
 #include <sstream>
 using namespace std;
-#define DEBUG 0
+#define DEBUG 1
 
 
 //============================================================================//
@@ -15,15 +15,23 @@ using namespace std;
 // Abstract class with the pure virtual function 'DisplayAction'
 struct Base {
 
+  /* 'Create' is a function with return type 'Base-pointer'
+
+     Weird?? recursion? also static, so only one Create function exists,
+     and no object is needed for the create function to be called*/
+
   static Base* Create(int id);
 
 	Base() {}
 
+  //
 	virtual ~Base() {}
 
-  static auto DisplayAction();
-  // virtual float* DisplayAction(float* value);
-  // virtual double* DisplayAction(double* value);
+  // maybe fix with tuple? tuple can be made with different types.
+  // assign nullptr to types that should not be used, then assign the value that
+  // is not a pullptr to a new value and voila :)
+  virtual tuple<int*,float*,double*> DisplayAction(void* value) = 0;
+  // virtual void pv() = 0;
 };
 
 
@@ -31,18 +39,18 @@ struct Base {
 
 
 class Derived1 : public Base {
-  int* intPtr;
 public:
-  Derived1(void* voidPtr) {
-    this->intPtr = static_cast<int*>(voidPtr);
+	Derived1() {
 		cout << "Derived1 created" << endl;
 	}
 	~Derived1() {
 		cout << "Derived1 destroyed" << endl;
 	}
 
-  int* DisplayAction() {
-    return intPtr;}
+  tuple<int*,float*,double*> DisplayAction(void* value) override {
+		cout << "Action from Derived1" << endl;
+    return make_tuple(static_cast<int*>(value), nullptr, nullptr);
+	}
 };
 
 
@@ -50,18 +58,18 @@ public:
 
 
 class Derived2 : public Base {
-  float* floatPtr;
 public:
-  Derived2(void* voidPtr) {
-    this->floatPtr = static_cast<float*>(voidPtr);
+	Derived2() {
 		cout << "Derived2 created" << endl;
 	}
 	~Derived2() {
 		cout << "Derived2 destroyed" << endl;
 	}
 
-  float* DisplayAction() {
-    return floatPtr;}
+  tuple<int*,float*,double*> DisplayAction(void* value) override {
+		cout << "Action from Derived2" << endl;
+    return make_tuple(nullptr, static_cast<float*>(value), nullptr);
+	}
 };
 
 
@@ -69,18 +77,18 @@ public:
 
 
 class Derived3 : public Base {
-  double* doublePtr;
 public:
-  Derived3(void* voidPtr) {
-    this->doublePtr = static_cast<double*>(voidPtr);
+	Derived3() {
 		cout << "Derived3 created" << endl;
 	}
 	~Derived3() {
 		cout << "Derived3 destroyed" << endl;
 	}
 
-  double* DisplayAction() {
-    return doublePtr;}
+  tuple<int*,float*,double*> DisplayAction(void* value) override {
+		cout << "Action from Derived3" << endl;
+    return make_tuple(nullptr, nullptr, static_cast<double*>(value));
+	}
 };
 
 
@@ -92,13 +100,13 @@ class Factory {
 public:
 
     // Method that creates the object
-  Base* create(char id, void* voidPtr) {
+  Base* create(char id) {
 
 		// Just expand the if-else ladder, if new Derived class is created
 		// User code need not be recompiled to create newly added class objects
-    if( id == 'i') {return new Derived1(voidPtr);}
-    else if( id == 'f') {return new Derived2(voidPtr);}
-    else if(id == 'd') {return new Derived3(voidPtr);}
+    if( id == 'i') {return new Derived1;}
+    else if( id == 'f') {return new Derived2;}
+    else if(id == 'd') {return new Derived3;}
 	}
 };
 
@@ -107,22 +115,31 @@ public:
 
 
 class User {
-
+private:
   Base* pBase;
-  char input;
 
 public:
-  User(char input, void* voidPtr) : pBase(nullptr) {
-    this->input = input;
+
+  // constructor inherits from
+  User(char input) : pBase(nullptr) {
+
+		// Receives an object of Base hierarchy at runtime
+		// int input;
+    //
+		// cout << "Enter ID (1, 2 or 3): ";
+		// cin >> input;
 
     while((input != 'i') && (input != 'f') && (input != 'd')) {
 			cout << "Enter ID (1, 2 or 3 only): ";
-			cin >> input;}
+			cin >> input;
+		}
 
+		// Get object from the "Virtual Constructor"
 		Factory f;
-    pBase = f.create(input,voidPtr);
+		pBase = f.create(input);
 	}
 
+    // When user object goes out of scope, delete pBase object
 	~User() {
 		if(pBase) {
 			delete pBase;
@@ -130,27 +147,26 @@ public:
 		}
 	}
 
-  // Base* Action() {
-  //   pBase->DisplayAction();
-  // }
+	// Delegates to actual object
+  // Is this really needed?
+  tuple<int*,float*,double*> Action(void* value) {
+    pBase->DisplayAction(value);
+	}
 };
 
-
-//============================================================================//
-
-
+// Consumer of User (UTILITY) class
 int main() {
   int value1 = 1;
-  // float value2 = 1.5;
+  float value2 = 1.5;
   void* value1Void = static_cast<void*>(&value1);
-  // void* value2Void = static_cast<void*>(&value2);
-  User* user = new User('i',value1Void);
-  // User* user2 = new User('f',value2Void);
-  user->DisplayAction(); // should be get
+  void* value2Void = static_cast<void*>(&value2);
 
+  User* user = new User('i');
+  User* user2 = new User('f');
 
-
-  // user2->Action(value2Void);
+	// Action required on any of Derived objects
+  user->Action(value1Void); // should be get
+  user2->Action(value2Void);
 
   /* NOTE:
   pointer stays assigned to the memory location of the User object,
@@ -165,8 +181,8 @@ int main() {
 #endif
 
 	delete user;
-  // delete user2;
-  // user2 = nullptr;
+  delete user2;
+  user2 = nullptr;
 
 #if DEBUG
     cout << "does user pointer still exist: " << user << endl;
